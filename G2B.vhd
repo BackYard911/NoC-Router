388fe5118562dfda431b963815498e05685363c7
walid <mohamed.walid1999@gmail.com>
2020-05-16 01:16:36 +0200

    typo in g2b

--- G2B.vhd
+++ G2B.vhd
@@ -1,18 +1,18 @@
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
 
-entity gbConverter is
-Port(
-      gray_in: in STD_LOGIC_VECTOR(3 downto 0);
-      bin_out: out STD_LOGIC_VECTOR(3 downto 0)
+ENTITY gbConverter IS
+      PORT (
+            gray_in : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+            bin_out : OUT STD_LOGIC_VECTOR(3 DOWNTO 0)
       );
-end gbConverter;
+END gbConverter;
 
-architecture Behavioral of gbConverter is
-begin
- bin_out(3)<=gray_in(3);
- bin_out(2)<=gray_in(3) xor gray_in(2);
- bin_out(1)<=gray_in(3) xor gray_in(2) xor gray_in(1);
- bin_out(0)<=gray_in(3) xor gray_in(2) xor gray_in(1) xor gray_in(0);
-end behavioral;
+ARCHITECTURE behavioral OF gbConverter IS
+BEGIN
+      bin_out(3) <= gray_in(3);
+      bin_out(2) <= gray_in(3) XOR gray_in(2);
+      bin_out(1) <= gray_in(3) XOR gray_in(2) XOR gray_in(1);
+      bin_out(0) <= gray_in(3) XOR gray_in(2) XOR gray_in(1) XOR gray_in(0);
+END behavioral;
