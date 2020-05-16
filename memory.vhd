2905d1dd8d437de7db6d5b64df6c0194af947354
BackYard911 <yousseftarek9801@gmail.com>
2020-05-16 02:36:48 +0200

    Update memory.vhd

--- memory.vhd
+++ memory.vhd
@@ -13,52 +13,33 @@ ENTITY memory IS
 		REA  :IN std_logic;
 		WEA  :IN std_logic;
 		CLKA  :IN std_logic;
-		CLKB  :IN std_logic
+		CLKB  :IN std_logic;
+		D_out : OUT std_logic_vector (data_width-1 downto 0)
 		 );
 END ENTITY;
 
 ARCHITECTURE syn OF memory IS
-	CONSTANT ram_depth :INTEGER := 2**address_width;
-	
-	SIGNAL D_out :std_logic_vector (data_width-1 downto 0);
 
-	TYPE ram IS ARRAY (INTEGER RANGE <>) OF std_logic_vector (data_width-1 DOWNTO 0);
-	SIGNAL mem: ram (0 TO ram_depth-1);
+	TYPE ram IS ARRAY (0 TO 7) OF std_logic_vector (data_width-1 DOWNTO 0);
+	SIGNAL mem: ram ;
 BEGIN
-	memory_write :
-	PROCESS(CLKA,CLKB) BEGIN
+
+	PROCESS(CLKA) IS
+	BEGIN
 		IF(rising_edge(CLKA)) THEN
 			IF (WEA = '1') THEN
-				mem(CONV_INTEGER(ADDRA)) <= D_in;
-			END IF;
-		END IF;
-		
-		IF(rising_edge(CLKB)) THEN
-			IF (WEA = '1') THEN
-				mem(CONV_INTEGER(ADDRB)) <= D_in;
-			END IF;
-		END IF;
-	END PROCESS;
-	 
-	memory_readA :
-	PROCESS (CLKA) BEGIN
-		IF (rising_edge(CLKA)) THEN
-			IF (WEA = '0' AND REA = '1') THEN
-				D_out <= mem(CONV_INTEGER(ADDRA));
-			ELSE
-				D_out <= (OTHERS => '0');
+				mem(conv_integer(ADDRA)) <= D_in;
 			END IF;
 		END IF;
 	END PROCESS;
 
-	memory_readB :
-	PROCESS (CLKB) BEGIN
+	PROCESS (CLKB) IS
+	BEGIN
 		IF (rising_edge(CLKB)) THEN
-			IF (WEA = '0' AND REA = '1') THEN
-				D_out <= mem(CONV_INTEGER(ADDRB));
-			ELSE
-				D_out <= (OTHERS => '0');
+			IF (REA = '1') THEN
+				D_out <= mem(conv_integer(ADDRB));
 			END IF;
 		END IF; 
-	END PROCESS;	
+	END PROCESS;
+	
 END ARCHITECTURE;
