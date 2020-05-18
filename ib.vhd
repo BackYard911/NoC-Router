a19794fde71a0fc21b16f27b5c11c64e36353435
youssefhassan21999 <49734123+youssefhassan21999@users.noreply.github.com>
2020-05-18 23:49:10 +0200

    ib by zoz

--- ib.vhd
+++ ib.vhd
@@ -1,1 +1,29 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 
+
+
+entity ib is
+    Port ( data_in : in  STD_LOGIC_VECTOR (7 downto 0);
+           clock : in  STD_LOGIC;
+           reset : in  STD_LOGIC;
+           clock_En : in  STD_LOGIC;
+           data_out : out  STD_LOGIC_VECTOR (7 downto 0));
+end ib;
+
+architecture input_Buffer_Arch of ib is
+
+begin
+    process(clock,reset)
+        begin
+            if reset = '1' then data_out <= "00000000";
+            elsif clock'event and clock = '1' then
+                if clock_En='1' then
+                    data_out <= data_in;
+                else
+                    null;
+            end if;
+            end if;
+        end process;
+
+end input_Buffer_Arch;
